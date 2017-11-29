1d0b95e183cd32db2382c90735e4ea31ac4cdc6a
soham-shah <sohamshah225@gmail.com>
2017-11-29 14:07:56 -0700

    destroying plane
    - added plane that trails the game object which can destroy objects that are tagged "destroyable" This solves some computation and viewport issues

---
+++ FlappyVR/Assets/Scripts/planeOfDeath.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class planeOfDeath : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.tag == "destroyable")
+		{
+			Destroy (other.gameObject);
+		}
+	}
+
+	void OnCollisionEnter(Collision other){
+		if (other.gameObject.tag == "destroyable") {
+			Destroy (other.gameObject);
+		}
+	}
+}
