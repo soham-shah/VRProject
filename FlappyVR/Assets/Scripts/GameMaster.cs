f7c565ba79edf9575f5b2e14ec75ff8fb4fc0eae
Conor Walsh <conorwalsh@rgnt2-76-188-dhcp.int.colorado.edu>
2017-12-01 15:21:43 -0700

    Added random wall generation to GameMaster

--- FlappyVR/Assets/Scripts/GameMaster.cs
+++ FlappyVR/Assets/Scripts/GameMaster.cs
@@ -4,20 +4,30 @@ using UnityEngine;
 
 public class GameMaster : MonoBehaviour {
 
-	public Transform obst1;
+	//public Transform obst1;
+	//public Transform MarioPipe;
 	public float zscenePos = 4;
+
+	public GameObject[] Obstacles; //Array for Obstacles
+
 
 
 	// Use this for initialization
 	void Start () {
-		while (zscenePos < 200) {
-			Instantiate (obst1, new Vector3 (-1, 0, zscenePos), obst1.rotation);
-			zscenePos += 4;
+		while (zscenePos < 40) {
+			//Instantiate (MarioPipe, new Vector3 (-1, 0, zscenePos), MarioPipe.rotation);
+			///Instantiate (Wall, new Vector3 (-1, 0, zscenePos), Wall.rotation);
+
+			int randomIndex = Random.Range(0, Obstacles.Length); //Randomly selects an object in our Array
+			Instantiate (Obstacles [randomIndex], new Vector3 (-1, 0, zscenePos), Quaternion.identity); //Instantiates our random obstacle
+
+			zscenePos += 10;
 		}
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		
+
 	}
 }
+
