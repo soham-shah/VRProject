02289e6c3c4c6bb5847ea7d6fce3eb0a798a0846
soham-shah <sohamshah225@gmail.com>
2017-12-08 11:58:16 -0700

    clean up code in moveOrb
    get rid of legacy gravity simulation code

--- FlappyVR/Assets/Scripts/moveOrb.cs
+++ FlappyVR/Assets/Scripts/moveOrb.cs
@@ -11,20 +11,12 @@ public class moveOrb : MonoBehaviour
     public KeyCode moveU;
     public KeyCode moveD;
 
-    public KeyCode flapper;
-
     public float horizVel = 0;
     public float vertVel = 0;
-    public int laneNum = 2;
     public bool controllLocked = false;
 
     public int forwardSpeed;
-    public int upSpeed;
-    public float gravityScale;
-    public static float globalGravity = -9.81f;
-
     private Rigidbody rb;
-    private bool didFlap = false;
 
     void Start()
     {
@@ -33,19 +25,17 @@ public class moveOrb : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKey(KeyCode.LeftArrow) && laneNum > 1 && !controllLocked)
+        if (Input.GetKey(KeyCode.LeftArrow) && !controllLocked)
         {
             horizVel = -2;
             StartCoroutine(stopHorizontalSlide());
-            //laneNum -= 1;
             controllLocked = true;
         }
 
-        if (Input.GetKey(KeyCode.RightArrow) && laneNum < 3 && !controllLocked)
+        if (Input.GetKey(KeyCode.RightArrow) && !controllLocked)
         {
             horizVel = 2;
             StartCoroutine(stopHorizontalSlide());
-            //laneNum += 1;
             controllLocked = true;
         }
         if (Input.GetKey(KeyCode.UpArrow) && !controllLocked)
@@ -60,33 +50,12 @@ public class moveOrb : MonoBehaviour
             StartCoroutine(stopVertSlide());
             controllLocked = true;
         }
-
-        if (Input.GetKeyDown(flapper) || Input.GetMouseButtonDown(0))
-        {
-            didFlap = true;
-        }
     }
 
     void FixedUpdate()
     {
-        //move forward fixed amount and add scalable gravity component
-        rb.velocity = new Vector3(horizVel, rb.velocity.y, rb.velocity.z);
-        rb.velocity = new Vector3(rb.velocity.x, vertVel, rb.velocity.z);
-        Vector3 gravity = globalGravity * gravityScale * Vector3.up;
-        //rb.AddForce(gravity, ForceMode.Acceleration);
+		rb.velocity = new Vector3(horizVel, vertVel, rb.velocity.z);
         rb.AddForce(Vector3.forward * 1);
-
-        //handle the up and down
-        if (didFlap)
-        {
-            rb.AddForce(Vector3.up * upSpeed);
-            didFlap = false;
-        }
-        else
-        {
-
-        }
-
     }
 
     void OnCollisionEnter(Collision other)
