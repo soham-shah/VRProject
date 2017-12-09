e464d4e1583ab453e8c0f034065a6f961fada890
soham-shah <sohamshah225@gmail.com>
2017-12-09 12:48:26 -0700

    Move Point of view
    - Shifted point of view to slightly in front of the ball hitbox
    - elongated the sides
    - shifted the sides and ground forward

---
+++ FlappyVR/Assets/Scripts/moveCamera.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveCamera : MonoBehaviour {
+
+	Vector3 offset;
+	private Transform player_pos;
+	void Start () {
+		GameObject player = GameObject.FindGameObjectWithTag ("player");
+
+		if(player == null) {
+			Debug.LogError("Couldn't find an object with tag 'Player'!");
+			return;
+		}
+
+		player_pos = player.transform;
+		offset = transform.position - player_pos.position;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (player_pos != null) {
+			transform.position = player_pos.position + offset;
+		}
+	}
+}
