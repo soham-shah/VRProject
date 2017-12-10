bd588285858dcbd8a466eef6c5770e78d00ef627
soham-shah <sohamshah225@gmail.com>
2017-12-10 16:36:21 -0700

    basic music visualization implementation
    - downloaded model that scales up only for the cube
    - wrote code to take music data into array
    - created new scene in which to experiment

---
+++ FlappyVR/Assets/Scripts/musicScripts/audioPeer.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof (AudioSource))]
+public class audioPeer : MonoBehaviour {
+
+	AudioSource _audiosource;
+	public static float[] _samples = new float[512];
+	public static float[] _freqBand = new float[8]; 
+	// Use this for initialization
+	void Start () {
+		_audiosource = GetComponent<AudioSource> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GetSpectrumAudioSource ();
+		MakeFreqBands ();
+	}
+
+	void GetSpectrumAudioSource (){
+		_audiosource.GetSpectrumData (_samples, 0, FFTWindow.Blackman);
+	}
+	void MakeFreqBands(){
+		int count = 0;
+		for (int i = 0; i < 8; i++) {
+			float average = 0;
+			int sampleCount = (int)Mathf.Pow (2, 1) * 2;
+
+			if (i == 7) {
+				sampleCount += 2;
+			}
+			for (int j = 0; j < sampleCount; j++) {
+				average += _samples [count] * (count + 1);
+				count++;
+			}
+			average /= count;
+			_freqBand [i] = average * 10;
+		}
+	}
+}
--- README.md
+++ README.md
@@ -7,3 +7,6 @@ Flappy Birds (Up and Down) adding left and right
 More obstacles (pipes, walls, moving obstacles (Connor)
 Rest(find code and try to moddify it)
 Envoirnment: Outside, changing textures as they progress
+
+Resources: 
+Used https://www.newgrounds.com/audio/listen/778683 for music
