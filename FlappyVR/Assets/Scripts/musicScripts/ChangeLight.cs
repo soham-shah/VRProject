751d9d6813811062f6e2f05e3a4c6a47634544d0
soham-shah <sohamshah225@gmail.com>
2017-12-12 15:04:10 -0700

    color changing
    - changing the color of the overall scene based on the music data output

---
+++ FlappyVR/Assets/Scripts/musicScripts/ChangeLight.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeLight : MonoBehaviour {
+
+	public float r;
+	public float g;
+	public float b;
+
+	private static Light myLight;
+
+	// Use this for initialization
+	void Start () {
+		myLight = GetComponent<Light>();
+//		myLight.color = Color.blue;
+		setColor();
+		InvokeRepeating("setColor", 0f, 1.0f);
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+	}
+
+	void setColor (){
+		r = interpolate(audioPeer._audioBandBuffer [0]);
+		g = interpolate(audioPeer._audioBandBuffer [1]);
+		b = interpolate(audioPeer._audioBandBuffer [2]);
+		myLight.color = new Color (audioPeer._audioBandBuffer [0],audioPeer._audioBandBuffer [1],audioPeer._audioBandBuffer [2],1f);
+	}
+
+	float interpolate(float OldValue){
+		float OldMax = 1;
+		float OldMin = 0;
+		float NewMax = 255;
+		float NewMin = 0;
+		float OldRange = (OldMax - OldMin);
+		float NewRange = (NewMax - NewMin); 
+		float NewValue = (((OldValue - OldMin) * NewRange) / OldRange) + NewMin;
+		return NewValue;
+	}
+}
